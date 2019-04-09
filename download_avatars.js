6e6506eb833e3bf6b893889f956aa210f6bfbe57
Danny <dongyunrhee@gmail.com>
2019-04-09 00:53:58 +0000

    Connecting functions, defining filepath and downloading avatar images.

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secret = require('./secrets.js');
+var fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -15,12 +16,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-function downloadImageByURL(url, filePath) {
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function(err){
+         console.log(err);
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .on('end',function (response) {
+        console.log('ended here');
+       })
+       .pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+  result.forEach(function(element){
+    //console.log(element.avatar_url);
+    var filePath = "./avatar/" + element.id + ".jpg";
+    //console.log(filePath);
+    downloadImageByURL(element.avatar_url, filePath);
+  });
 });
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
 
